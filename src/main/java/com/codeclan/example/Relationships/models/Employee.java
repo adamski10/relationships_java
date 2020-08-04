16529f35b6c28ac027c856047200b17a91a85667
AdamK <44851323+adamski10@users.noreply.github.com>
2020-08-04 11:16:28 +0200

    file structure in place

---
+++ src/main/java/com/codeclan/example/Relationships/models/Employee.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.Relationships.models;
+
+public class Employee {
+    String firstName;
+    String lastName;
+    int employeeNumber;
+    ArrayList<Project> projects;
+}
---
+++ src/main/java/com/codeclan/example/Relationships/models/Project.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.Relationships.models;
+
+public class Project {
+}
